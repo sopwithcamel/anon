c1fc0ef93cec4a56fd1573979b80cb3450df2e08
Hrishikesh Amur <amur@gatech.edu>
2013-01-19 17:52:49 -0500

    Adding a tokenizer for kmers

--- lib/tokenizers.hh
+++ lib/tokenizers.hh
@@ -69,7 +69,7 @@ struct split_record {
 
         // if it is not the first split, then move forward until we hit a
         // newline
-        if (ma_->split_start_offset > 0) {
+        if (ma_->chunk_start_offset > 0) {
             for (; *str_ != '\n'; ++str_);
             ++str_;
         }
@@ -121,6 +121,88 @@ struct split_record {
     split_t* ma_;
     char* str_;
     char stop_[256];
+    char* saveptr1;
+    size_t len_;
+    bool bytewise_;
+    size_t overlap_;
+};
+
+struct split_kmer {
+    split_kmer(split_t *ma, size_t overlap, uint32_t k) :
+            ma_(ma), k_(k),
+            read_(NULL), read_offset_(0), len_(0), bytewise_(false),
+            overlap_(overlap) {
+        assert(ma_ && ma_->data);
+        str_ = ma_->data;
+
+        // if it is not the first split, then move forward until we hit a
+        // newline. All kmers that start before the newline are the
+        // responsibility of the previous split
+        if (ma_->split_start_offset > 0) {
+            for (; *str_ != '\n'; ++str_);
+            ++str_;
+        }
+        // if this has landed us on a header line then move forward
+        // another line
+        if (*str_ == '>') {
+            for (; *str_ != '\n'; ++str_);
+            ++str_;
+        }
+    }
+
+    bool fill(char *key, size_t maxlen, size_t &klen) {
+        char* spl;
+        size_t chunk_length = ma_->chunk_end_offset -
+            ma_->chunk_start_offset;
+
+        // get the next read
+        if (!read_ || read_offset_ + k_ == read_length_) {
+            read_ = strtok_r(str_, ">", &saveptr1);
+            str_ = NULL;
+            if (!read_)
+                return false;
+            read_length_ = strlen(read_);
+            read_offset_ = 0;
+
+            bool are_we_in_header = false;
+            for (int test_len = 0; test_len < 4; ++test_len) {
+                char c = *(read_ + test_len);
+                if (c != 'A' && c != 'C' && c != 'G' && c != 'T') {
+                    are_we_in_header = true;
+                    break;
+                }
+            }
+            if (are_we_in_header) {
+                // jump forward until newline
+                for (; *(read_ + read_offset_) != '\n'; ++read_offset_);
+                ++read_offset_;
+            }
+        }
+
+        klen = 0;
+        size_t ind = read_offset_++;
+        while (klen < k_) {
+            char c = read_[ind++];
+            if (c != '\n' && c != 'N')
+                key[klen++] = c;
+        }
+
+        // if we're close to the overlap zone, switch to bytewise (and
+        // non-modifying) mode
+        if (read_ + read_offset_ > ma_->data + chunk_length) {
+            if (read_[read_offset_] == '\n')
+                return false;
+        }
+        return true;
+    }
+
+  private:
+    split_t* ma_;
+    uint32_t k_;
+    char* str_;
+    char* read_;
+    size_t read_length_;
+    size_t read_offset_;
     char* saveptr1;
     size_t len_;
     bool bytewise_;
